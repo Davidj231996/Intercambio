6a878d3eb487108f0556080eb34a1f43b007da93
David Jesus Perez Rubiales <alerodiez8dj@hotmail.com>
2022-02-19 11:19:39 +0100

    Se ha añadido los atributos Usuario y Categoria

--- src/Preferencia/Application/Create/PreferenciaCreator.php
+++ src/Preferencia/Application/Create/PreferenciaCreator.php
@@ -2,17 +2,19 @@
 
 namespace App\Preferencia\Application\Create;
 
+use App\Categoria\Domain\Categoria;
 use App\Preferencia\Domain\Preferencia;
 use App\Preferencia\Domain\PreferenciaRepository;
+use App\Usuario\Domain\Usuario;
 
 class PreferenciaCreator
 {
     public function __construct(private PreferenciaRepository $repository)
     {}
 
-    public function create(int $usuarioId, int $categoriaId): void
+    public function create(Usuario $usuario, Categoria $categoria): void
     {
-        $preferencia = Preferencia::create(null, $usuarioId, $categoriaId);
+        $preferencia = Preferencia::create(null, $usuario, $categoria);
         $this->repository->save($preferencia);
     }
 }
