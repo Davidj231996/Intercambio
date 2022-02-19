de9d31b333882d7d7a274225307d8ce54743e116
David Jesus Perez Rubiales <alerodiez8dj@hotmail.com>
2022-02-19 16:58:49 +0100

    Formulario actualización datos usuario

---
+++ src/Form/Usuario/UsuarioUpdateType.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Form\Usuario;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class UsuarioUpdateType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('nombre', TextType::class)
+            ->add('apellidos', TextType::class)
+            ->add('telefono', TelType::class)
+            ->add('email', EmailType::class)
+            ->add('save', SubmitType::class);
+    }
+}
