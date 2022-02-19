365d8880570e45d19b0b4b81f6fab56f58529e29
David Jesus Perez Rubiales <alerodiez8dj@hotmail.com>
2022-02-19 17:00:07 +0100

    Formulario creación imagen de objeto

---
+++ src/Form/Imagen/ImagenObjetoCreateType.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Form\Imagen;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class ImagenObjetoCreateType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('descripcion', TextType::class)
+            ->add('imagen', FileType::class)
+            ->add('objeto', HiddenType::class)
+            ->add('save', SubmitType::class);
+    }
+}
