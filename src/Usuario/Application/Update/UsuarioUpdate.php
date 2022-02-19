5a1c42a05296536bbdd92455cd8c720120149941
David Jesus Perez Rubiales <alerodiez8dj@hotmail.com>
2022-02-19 17:03:01 +0100

    Se ha eliminado la contraseña como atributo, ya que no se va a usar en update

--- src/Usuario/Application/Update/UsuarioUpdate.php
+++ src/Usuario/Application/Update/UsuarioUpdate.php
@@ -5,21 +5,17 @@ namespace App\Usuario\Application\Update;
 
 use App\Usuario\Domain\UsuarioFinder;
 use App\Usuario\Domain\UsuarioRepository;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UsuarioUpdate
 {
-    private UsuarioFinder $finder;
-
-    public function __construct(private UsuarioRepository $repository, private UserPasswordHasherInterface $passwordHasher)
+    public function __construct(private UsuarioRepository $repository, private UsuarioFinder $finder)
     {
-        $this->finder = new UsuarioFinder($repository);
     }
 
-    public function update(int $id, string $alias, string $nombre, string $apellidos, string $telefono, string $email, string $password): void
+    public function update(int $id, string $alias, string $nombre, string $apellidos, string $telefono, string $email): void
     {
         $usuario = $this->finder->__invoke($id);
-        $usuario->update($alias, $nombre, $apellidos, $telefono, $email, $this->passwordHasher->hashPassword($usuario, $password));
+        $usuario->update($alias, $nombre, $apellidos, $telefono, $email);
         $this->repository->save($usuario);
     }
 }
