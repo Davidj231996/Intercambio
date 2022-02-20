212fe9078130113291f12ca61a54318e86948927
David Jesus Perez Rubiales <alerodiez8dj@hotmail.com>
2022-02-20 16:25:11 +0100

    Se ha añadido la funcionalidad para que se pueda contraer el menú lateral en la base de Usuario

--- public/js/baseUsuario.js
+++ public/js/baseUsuario.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
-    $('span.nav-link').click(function () {
+    $('#menuUsuario a.nav-link').click(function () {
+        $('#menuUsuario li.active').removeClass('active');
         $('div.active').fadeOut().removeClass('active');
         $("#" + this.getAttribute('content')).fadeIn('slow').addClass('active');
+    });
+
+    // Toggle the side navigation
+    $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
+        $("body").toggleClass("sidebar-toggled");
+        $(".sidebar").toggleClass("toggled");
+        if ($(".sidebar").hasClass("toggled")) {
+            $('.sidebar .collapse').collapse('hide');
+        };
     });
 });
