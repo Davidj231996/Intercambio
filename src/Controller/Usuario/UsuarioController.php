b6032621d215d049b5b298cd13cc72f8876f23a3
David Jesus Perez Rubiales <alerodiez8dj@hotmail.com>
2022-02-20 16:30:49 +0100

    Se ha modificado para que sirve solo para mostrar los datos de otros usuarios

--- src/Controller/Usuario/UsuarioController.php
+++ src/Controller/Usuario/UsuarioController.php
@@ -2,9 +2,6 @@
 
 namespace App\Controller\Usuario;
 
-use App\Form\Direccion\DireccionUpdateType;
-use App\Form\Usuario\UsuarioPasswordType;
-use App\Form\Usuario\UsuarioUpdateType;
 use App\Usuario\Domain\UsuarioFinder;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -24,18 +21,8 @@ class UsuarioController extends AbstractController
     public function usuario(int $usuarioId): Response
     {
         $usuario = $this->finder->__invoke($usuarioId);
-        $usuarioForm = $this->createForm(UsuarioUpdateType::class, $usuario);
-        $passwordForm = $this->createForm(UsuarioPasswordType::class, [
-            'usuario' => $usuario
-        ], [
-            'action' => $this->generateUrl('updatePassword')
-        ]);
-        $direccionForm = $this->createForm(DireccionUpdateType::class, $usuario->direccion());
         return $this->render('usuario/usuario.html.twig', [
-            'usuario' => $usuario,
-            'usuarioForm' => $usuarioForm->createView(),
-            'passwordForm' => $passwordForm->createView(),
-            'direccionForm' => $direccionForm->createView()
+            'usuario' => $usuario
         ]);
     }
 }
