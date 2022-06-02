96c701dc9b05f2c1b5cba32b09dd2c87cf27a4cc
Davidj231996 <alerodiez8dj@hotmail.com>
2022-06-02 19:53:09 +0200

    Controlador Aceptar Reserva

---
+++ src/Controller/Reserva/ReservaAceptarController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller\Reserva;
+
+use App\Reserva\Application\Aceptar\ReservaAceptar;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ReservaAceptarController extends AbstractController
+{
+    public function __construct(private ReservaAceptar $reservaAceptar) {}
+
+    /**
+     * @Route("reservaaceptar/{reservaId}", name="reserva_aceptar")
+     * @param Request $request
+     * @param $reservaId
+     * @return mixed
+     */
+    public function aceptar(Request $request, $reservaId) {
+        $this->reservaAceptar->update($reservaId);
+        return $this->redirectToRoute($request->request->get('referer'));
+    }
+}
