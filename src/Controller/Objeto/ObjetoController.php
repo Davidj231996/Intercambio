564c4e912846635800b41b02c355ea91bc33cdf0
Davidj231996 <alerodiez8dj@hotmail.com>
2022-06-02 17:56:26 +0200

    Se ha añadido para que compruebe si el usuario actual tiene reservado el objeto que se pasa por argumentos

--- src/Controller/Objeto/ObjetoController.php
+++ src/Controller/Objeto/ObjetoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Objeto;
 
+use App\Objeto\Application\ReservadoUsuario\ObjetoReservadoUsuario;
 use App\Objeto\Domain\ObjetoFinder;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,7 +10,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ObjetoController extends AbstractController
 {
-    public function __construct(private ObjetoFinder $finder)
+    public function __construct(private ObjetoFinder $finder, private ObjetoReservadoUsuario $objetoReservadoUsuario)
     {
     }
 
@@ -20,8 +21,10 @@ class ObjetoController extends AbstractController
      */
     public function objeto(int $objetoId): Response
     {
+        $objeto = $this->finder->__invoke($objetoId);
         return $this->render('objeto/objeto.html.twig', [
-            'objeto' => $this->finder->__invoke($objetoId)
+            'objeto' => $objeto,
+            'reservado' => $this->objetoReservadoUsuario->estaReservado($objeto, $this->getUser())
         ]);
     }
 }
