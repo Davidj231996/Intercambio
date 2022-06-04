42fa8d75f4bca55d540c248f3d365cb37f585b05
Davidj231996 <alerodiez8dj@hotmail.com>
2022-06-04 19:09:14 +0200

    Se ha modificado para que se añada el usuario por parametro sin tener que buscarlo

--- src/Usuario/Application/Update/UsuarioUpdate.php
+++ src/Usuario/Application/Update/UsuarioUpdate.php
@@ -3,21 +3,19 @@
 namespace App\Usuario\Application\Update;
 
 
+use App\Usuario\Domain\Usuario;
 use App\Usuario\Domain\UsuarioFinder;
 use App\Usuario\Domain\UsuarioRepository;
 
 class UsuarioUpdate
 {
-    private UsuarioFinder $finder;
-
     public function __construct(private UsuarioRepository $repository)
     {
         $this->finder = new UsuarioFinder($repository);
     }
 
-    public function update(int $id, string $nombre, string $apellidos, string $telefono, string $email): void
+    public function update(Usuario $usuario, string $nombre, string $apellidos, string $telefono, string $email): void
     {
-        $usuario = $this->finder->__invoke($id);
         $usuario->update($nombre, $apellidos, $telefono, $email);
         $this->repository->save($usuario);
     }
