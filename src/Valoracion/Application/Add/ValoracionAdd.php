7448eb90ca7cfd5a489aac778292d0dbba56ccd8
Davidj231996 <alerodiez8dj@hotmail.com>
2022-06-04 14:24:16 +0200

    Se ha modificado la aplicación de añadir valoración, para que compruebe si hay valoración y añada, y si no hay la cree llamando a la aplicación de creación

--- src/Valoracion/Application/Add/ValoracionAdd.php
+++ src/Valoracion/Application/Add/ValoracionAdd.php
@@ -3,18 +3,23 @@
 namespace App\Valoracion\Application\Add;
 
 use App\Usuario\Domain\Usuario;
+use App\Valoracion\Application\Create\ValoracionCreate;
 use App\Valoracion\Domain\ValoracionRepository;
 
 class ValoracionAdd
 {
-    public function __construct(private ValoracionRepository $repository)
+    public function __construct(private ValoracionRepository $repository, private ValoracionCreate $valoracionCreate)
     {}
 
     public function add(Usuario $usuario, float $valor): void
     {
         $valoracion = $this->repository->search($usuario->id());
-        $nuevoValor = ($valoracion->valoracionTotal() + $valor) / ($valoracion->totales() + 1);
-        $valoracion->add($nuevoValor);
-        $this->repository->save($valoracion);
+        if ($valoracion) {
+            $nuevoValor = ($valoracion->valoracionTotal() + $valor) / ($valoracion->totales() + 1);
+            $valoracion->add($nuevoValor);
+            $this->repository->save($valoracion);
+        } else {
+            $this->valoracionCreate->create($usuario, $valor);
+        }
     }
 }
