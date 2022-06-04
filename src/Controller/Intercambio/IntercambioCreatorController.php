ba72976ef7f77562b03f22c09b286039f790fe18
Davidj231996 <alerodiez8dj@hotmail.com>
2022-06-04 18:01:53 +0200

    Se ha modificado para que funcione con el formulario de creación de intercambio

--- src/Controller/Intercambio/IntercambioCreatorController.php
+++ src/Controller/Intercambio/IntercambioCreatorController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller\Intercambio;
 
+use App\Form\Intercambio\IntercambioCreateType;
 use App\Intercambio\Application\Create\IntercambioCreate;
 use App\Objeto\Domain\ObjetoFinder;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IntercambioCreatorController extends AbstractController
@@ -15,30 +18,44 @@ class IntercambioCreatorController extends AbstractController
     }
 
     /**
-     * @Route("/intercambiocreate/{objetoIntercambioId}/{objetoIntercambiarId}", name="intercambio_create")
-     * @param $objetoIntercambioId
+     * @Route("/intercambiocreate/{objetoIntercambiarId}", name="intercambio_create")
+     * @param Request $request
      * @param $objetoIntercambiarId
-     * @return RedirectResponse
+     * @return Response
      */
-    public function create($objetoIntercambioId, $objetoIntercambiarId)
+    public function create(Request $request, $objetoIntercambiarId): Response
     {
-        $objetoIntercambio = $this->objetoFinder->__invoke($objetoIntercambioId);
         $objetoIntercambiar = $this->objetoFinder->__invoke($objetoIntercambiarId);
 
-        if ($this->getUser() != $objetoIntercambio->usuario() && $this->getUser() == $objetoIntercambiar->usuario()
-            && $objetoIntercambio->reservado() && $objetoIntercambio->reserva()->usuario() == $this->getUser()) {
-            try {
-                $this->intercambioCreate->create($objetoIntercambio, $objetoIntercambiar, $objetoIntercambio->usuario(), $objetoIntercambiar->usuario());
-            } catch (\Exception $exception) {
-                return $this->redirectToRoute('objeto', [
-                    'objetoId' => $objetoIntercambioId
-                ]);
-            }
+        if (!$objetoIntercambiar->reservado() || $objetoIntercambiar->reserva()->usuario() != $this->getUser()) {
+            return $this->redirectToRoute('objeto', [
+                'objetoId' => $objetoIntercambiarId
+            ]);
         }
 
-        return $this->redirectToRoute('objeto', [
-            'objetoId' => $objetoIntercambioId,
-            'success' => 'Petición intercambio creada'
+        $form = $this->createForm(IntercambioCreateType::class, [
+            'usuario' => $this->getUser()
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            try {
+                $this->intercambioCreate->create($data['objeto'], $objetoIntercambiar);
+            } catch (Exception $exception) {
+                return $this->renderForm('intercambio/create.html.twig', [
+                    'form' => $form,
+                    'error' => $exception->getMessage()
+                ]);
+            }
+            return $this->redirectToRoute('objeto', [
+                'objetoId' => $objetoIntercambiarId
+            ]);
+        }
+
+        return $this->renderForm('intercambio/create.html.twig', [
+            'form' => $form
         ]);
     }
 }
