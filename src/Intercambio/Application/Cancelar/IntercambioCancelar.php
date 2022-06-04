b7ba9823cbea3f925d88d41c70a3f1fc43253868
Davidj231996 <alerodiez8dj@hotmail.com>
2022-06-04 18:40:02 +0200

    Se ha modificado para que cancele la reserva asociada, y marque como pendiente el objeto de la reserva

--- src/Intercambio/Application/Cancelar/IntercambioCancelar.php
+++ src/Intercambio/Application/Cancelar/IntercambioCancelar.php
@@ -4,20 +4,28 @@ namespace App\Intercambio\Application\Cancelar;
 
 use App\Intercambio\Domain\Intercambio;
 use App\Intercambio\Domain\IntercambioRepository;
+use App\Objeto\Domain\ObjetoRepository;
+use App\Reserva\Domain\ReservaRepository;
 use DateTime;
 
 class IntercambioCancelar
 {
-    public function __construct(private IntercambioRepository $repository)
+    public function __construct(private IntercambioRepository $repository, private ObjetoRepository $objetoRepository, private ReservaRepository $reservaRepository)
     {
     }
 
-    public function cancelar(int $id):void
+    public function cancelar(int $id): void
     {
         $intercambio = $this->repository->search($id);
         $now = new DateTime();
         $intercambio->updateIntercambio(Intercambio::ESTADO_CANCELADO, $now);
         $intercambio->updateIntercambiar(Intercambio::ESTADO_CANCELADO, $now);
         $this->repository->save($intercambio);
+
+        $intercambio->objetoIntercambiar()->reserva()->cancelar();
+        $this->reservaRepository->save($intercambio->objetoIntercambiar()->reserva());
+
+        $intercambio->objetoIntercambiar()->pendiente();
+        $this->objetoRepository->save($intercambio->objetoIntercambiar());
     }
 }
