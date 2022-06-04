377543528fb9f81e551fd4a14fb050aa2138ebd0
Davidj231996 <alerodiez8dj@hotmail.com>
2022-06-04 16:31:26 +0200

    Se han añadido aplicaciones para marcar como canceladas, finalizadas y enviadas el estado intercambiar

---
+++ src/Intercambio/Application/Finalizar/IntercambioFinalizarIntercambiar.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Intercambio\Application\Finalizar;
+
+use App\Intercambio\Domain\Intercambio;
+use App\Intercambio\Domain\IntercambioRepository;
+use DateTime;
+
+class IntercambioFinalizarIntercambiar
+{
+    public function __construct(private IntercambioRepository $repository)
+    {
+    }
+
+    public function update(int $id):void
+    {
+        $intercambio = $this->repository->search($id);
+        $now = new DateTime();
+        $intercambio->updateIntercambiar(Intercambio::ESTADO_FINALIZADO, $now);
+    }
+}
