1c8cbfb222b7a1550054fe6900dcce99208c0925
Davidj231996 <alerodiez8dj@hotmail.com>
2022-06-04 14:23:15 +0200

    Se ha modificado el controlador para que funcione con un formulario

--- src/Controller/Valoracion/ValoracionAddController.php
+++ src/Controller/Valoracion/ValoracionAddController.php
@@ -2,24 +2,40 @@
 
 namespace App\Controller\Valoracion;
 
+use App\Form\Valoracion\ValoracionCreateType;
+use App\Usuario\Domain\UsuarioFinder;
 use App\Valoracion\Application\Add\ValoracionAdd;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ValoracionAddController extends AbstractController
 {
-    public function __construct(private ValoracionAdd $add)
-    {}
+    public function __construct(private ValoracionAdd $valoracionAdd, private UsuarioFinder $usuarioFinder)
+    {
+    }
 
     /**
-     * @Route("/valoracionAdd/{valor}")
-     * @param float $valor
+     * @Route("/valoracionAdd/{usuarioId}", name="valoracion_add")
+     * @param Request $request
+     * @param $usuarioId
      * @return Response
      */
-    public function valoracionAdd(float $valor): Response
+    public function add(Request $request, $usuarioId): Response
     {
-        $this->add->add($this->getUser(), $valor);
-        return $this->render('index.html.twig');
+        $form = $this->createForm(ValoracionCreateType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $this->valoracionAdd->add($this->usuarioFinder->__invoke($usuarioId), $data['valor']);
+            return $this->redirectToRoute('usuario', [
+                'usuarioId' => $usuarioId
+            ]);
+        }
+        return $this->renderForm('valoracion/add.html.twig', [
+            'form' => $form
+        ]);
     }
 }
