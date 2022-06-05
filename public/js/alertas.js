7de3d5ac2a9d72e18e7dd5895efdd02f909fcd8c
Davidj231996 <alerodiez8dj@hotmail.com>
2022-06-05 17:13:50 +0200

    Se han añadido para tener alerta de error y de éxito

--- public/js/alertas.js
+++ public/js/alertas.js
@@ -1,10 +1,9 @@
 // The main class
-var AlertBox = function(id, option) {
-    this.show = function(msg) {
-        if (msg === ''  || typeof msg === 'undefined' || msg === null) {
+var AlertBox = function (id, option) {
+    this.show = function (msg) {
+        if (msg === '' || typeof msg === 'undefined' || msg === null) {
             throw '"msg parameter is empty"';
-        }
-        else {
+        } else {
             var alertArea = document.querySelector(id);
             var alertBox = document.createElement('DIV');
             var alertContent = document.createElement('DIV');
@@ -20,22 +19,25 @@ var AlertBox = function(id, option) {
                 alertBox.appendChild(alertClose);
             }
             alertArea.appendChild(alertBox);
-            alertClose.addEventListener('click', function(event) {
+            alertClose.addEventListener('click', function (event) {
                 event.preventDefault();
                 alertClass.hide(alertBox);
             });
-            if (!option.persistent) {
-                var alertTimeout = setTimeout(function() {
-                    alertClass.hide(alertBox);
-                    clearTimeout(alertTimeout);
-                }, option.closeTime);
+            var alertTimeout = setTimeout(function () {
+                alertClass.hide(alertBox);
+                clearTimeout(alertTimeout);
+            }, option.closeTime);
+            if (option.success) {
+                alertBox.classList.add('alert-success');
+            } else {
+                alertBox.classList.add('alert-error');
             }
         }
     };
 
-    this.hide = function(alertBox) {
+    this.hide = function (alertBox) {
         alertBox.classList.add('hide');
-        var disperseTimeout = setTimeout(function() {
+        var disperseTimeout = setTimeout(function () {
             alertBox.parentNode.removeChild(alertBox);
             clearTimeout(disperseTimeout);
         }, 500);
@@ -43,8 +45,16 @@ var AlertBox = function(id, option) {
 };
 
 // Sample invoke
-var alertbox = new AlertBox('#alert-area', {
+var alertboxSucces = new AlertBox('#alert-area', {
     closeTime: 4000,
     persistent: false,
-    hideCloseButton: false
+    hideCloseButton: false,
+    success: true
+});
+
+var alertboxError = new AlertBox('#alert-area', {
+    closeTime: 4000,
+    persistent: false,
+    hideCloseButton: false,
+    success: false
 });
