57c9a3bb64dc29f1bd5b3d8a8b374ba4d77fb1f0
Davidj231996 <alerodiez8dj@hotmail.com>
2022-09-07 17:28:37 +0200

    Entidad CategoriaObjeto

--- src/CategoriaObjeto/Application/AddCategorias/AddCategoriasObjeto.php
+++ src/CategoriaObjeto/Application/AddCategorias/AddCategoriasObjeto.php
@@ -2,7 +2,6 @@
 
 namespace App\CategoriaObjeto\Application\AddCategorias;
 
-use App\Categoria\Domain\CategoriaFinder;
 use App\CategoriaObjeto\Domain\CategoriaObjeto;
 use App\CategoriaObjeto\Domain\CategoriaObjetoRepository;
 use App\Objeto\Domain\Objeto;
@@ -11,9 +10,10 @@ use Doctrine\Common\Collections\ArrayCollection;
 class AddCategoriasObjeto
 {
     public function __construct(private CategoriaObjetoRepository $repository)
-    {}
+    {
+    }
 
-    public function addCategoriasObjeto(ArrayCollection $categorias, Objeto $objeto)
+    public function addCategoriasObjeto(ArrayCollection $categorias, Objeto $objeto): void
     {
         foreach ($categorias as $categoria) {
             $categoriaObjeto = CategoriaObjeto::create(null, $objeto, $categoria);
--- src/CategoriaObjeto/Application/UpdateCategorias/UpdateCategoriasObjeto.php
+++ src/CategoriaObjeto/Application/UpdateCategorias/UpdateCategoriasObjeto.php
@@ -5,14 +5,14 @@ namespace App\CategoriaObjeto\Application\UpdateCategorias;
 use App\CategoriaObjeto\Domain\CategoriaObjeto;
 use App\CategoriaObjeto\Domain\CategoriaObjetoRepository;
 use App\Objeto\Domain\Objeto;
-use Doctrine\Common\Collections\ArrayCollection;
 
 class UpdateCategoriasObjeto
 {
     public function __construct(private CategoriaObjetoRepository $repository)
-    {}
+    {
+    }
 
-    public function updateCategoriasObjeto(array $categorias, Objeto $objeto)
+    public function updateCategoriasObjeto(array $categorias, Objeto $objeto): void
     {
         foreach ($objeto->categorias() as $categoriaObjeto) {
             $this->repository->delete($categoriaObjeto);
