ce43f5e9b1089d1f9456b100ccb6464cc1754473
Davidj231996 <alerodiez8dj@hotmail.com>
2022-09-07 20:18:53 +0200

    Formularios Intercambio

--- src/Form/Intercambio/IntercambioCreateType.php
+++ src/Form/Intercambio/IntercambioCreateType.php
@@ -3,20 +3,32 @@
 namespace App\Form\Intercambio;
 
 use App\Objeto\Domain\Objeto;
+use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Security;
 
 class IntercambioCreateType extends AbstractType
 {
+    public function __construct(private Security $security)
+    {
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('objeto', EntityType::class, [
                 'class' => Objeto::class,
-                'choices' => $options['data']['usuario']->objetos(),
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('objeto')
+                        ->where('objeto.estado = :estado')
+                        ->andWhere('objeto.usuario = :usuario')
+                        ->setParameter('estado', Objeto::ESTADO_PENDIENTE)
+                        ->setParameter('usuario', $this->security->getUser());
+                },
                 'choice_label' => 'nombre',
                 'multiple' => false,
                 'attr' => [
