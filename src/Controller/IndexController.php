d3d2390b77fcc9cf36e191c2697392bb515f9c8c
Davidj231996 <alerodiez8dj@hotmail.com>
2022-09-07 20:24:15 +0200

    Controladores Portada

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Objeto\Application\ObjetoIndexFinder\ObjetoIndexFinder;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
+    public function __construct(private ObjetoIndexFinder $objetoIndexFinder)
+    {
+    }
+
     /**
      * @Route("/", name="index")
      */
     public function index(): Response
     {
-        return $this->render('index.html.twig');
+        return $this->render('index.html.twig', [
+            'objetos' => $this->objetoIndexFinder->__invoke()
+        ]);
     }
 }
