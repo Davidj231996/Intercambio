130e2be7c7295f1b6e2a3b4148ca7e959d6dd0b1
Davidj231996 <alerodiez8dj@hotmail.com>
2022-09-11 20:47:07 +0200

    Se ha añadido para marcar que el usuario tiene un chat sin leer

--- src/Chat/Application/Create/ChatCreate.php
+++ src/Chat/Application/Create/ChatCreate.php
@@ -4,20 +4,28 @@ namespace App\Chat\Application\Create;
 
 use App\Chat\Domain\Chat;
 use App\Chat\Domain\ChatRepository;
+use App\Usuario\Application\ChatNoLeido\UsuarioChatNoLeido;
 use App\Usuario\Domain\Usuario;
 use App\Usuario\Domain\UsuarioFinder;
 use DateTime;
 
 class ChatCreate
 {
-    public function __construct(private ChatRepository $chatRepository, private UsuarioFinder $usuarioFinder)
+    public function __construct(
+        private ChatRepository $chatRepository,
+        private UsuarioFinder $usuarioFinder,
+        private UsuarioChatNoLeido $usuarioChatNoLeido
+    )
     {
     }
 
     public function create(Usuario $usuario1, int $idUsuario2): void
     {
         $now = new DateTime();
-        $chat = Chat::create(null, $usuario1, $this->usuarioFinder->__invoke($idUsuario2), $now);
+        $usuario2 = $this->usuarioFinder->__invoke($idUsuario2);
+        $chat = Chat::create(null, $usuario1, $usuario2, $now);
         $this->chatRepository->save($chat);
+
+        $this->usuarioChatNoLeido->marcarNoLeido($usuario2);
     }
 }
